5c5c00694cd4c5122f22d4b4ccd53e7c02ee5dec
ehime <dodomeki@gmail.com>
2014-12-03 17:53:45 -0800

    Adding a GoLang isntaller for Darwin

---
+++ go-installer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! hash brew; then
+	echo 'Installing Homebrew...'
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update && brew upgrade
+
+brew doctor
+brew install hg go
+
+[ -d $HOME/go ] || mkdir $HOME/go
+echo -e 'export GOPATH=$HOME/go\nexport PATH="$GOPATH/bin:$PATH"\nexport PATH="$PATH:/usr/local/opt/go/libexec/bin"' >> .bash_profile
+echo "Installation complete...."
