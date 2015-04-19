4a2f8727a1a78edf3907f73cb965401162467e33
ehime <dodomeki@gmail.com>
2015-04-19 13:53:08 -0700

    Migrating in from more current bashtools version

--- y2m.sh
+++ y2m.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Youtube to MP3 Bash Script
- 
-# CPR : Jd Daniel :: Ehime-ken
-# MOD : 2014-12-05 @ 10:54:55
-# VER : Version 4 (OSX Darwin)
 
-# DEP : http://goo.gl/7YeHib
+# CPR : Jd Daniel :: Ehime-ken
+# MOD : 2015-01-21 @ 16:11:44
+# VER : Version 5 (OSX Darwin)
+
+# REQ : http://developer.echonest.com/docs/v4/song.html
+
 
 # no long-opts supported except --help
 while getopts 'v:d:-:' OPT; do
@@ -13,87 +14,60 @@ while getopts 'v:d:-:' OPT; do
     v) address=$OPTARG;;
     d) dir=$OPTARG;;
     -) #long option
-       case $OPTARG in
+    case $OPTARG in
 
-         help) 
-           echo -e 'Welcome to Youtube to MP3!\n\nUsage:\nLong:  y2m -d {directory} -v {ex: http://www.youtube.com/watch?v=oHg5SJYRHA0‎}\nShort: y2m {ex: http://www.youtube.com/watch?v=oHg5SJYRHA0‎}\n'
-           exit;;
+      help) echo 'Long:  y2m -d {directory} -v {ex: http://www.youtube.com/watch?v=oHg5SJYRHA0‎}'
+            echo 'Short: y2m {ex: http://www.youtube.com/watch?v=oHg5SJYRHA0‎}'
+            exit
+      ;;
 
-       esac;;
+    esac;;
   esac
 done
 
 cd /tmp
 
 # if short order (y2m http://addy.com)
-if [ -e $address ]; then
-    address=$1
-
-    # default to /home/{user}/Music
-    dir="/Users/${USER}/Music/"
-fi
+[ -e $address ] && {
+  # default to /home/{user}/Music
+  address=$1; dir="/Users/${USER}/Music/"
+}
 
 regex='v=(.*)'
 
-if [[ $address =~ $regex ]]; then
+[[ $address =~ $regex ]] && {
+  video_id=${BASH_REMATCH[1]}
+  video_id=$(echo $video_id | cut -d'&' -f1)
 
-    video_id=${BASH_REMATCH[1]}
-    video_id=$(echo $video_id | cut -d'&' -f1)
+  filename=$(basename "$FILEPATH")
+  extension="${filename##*.}"
 
-  FILENAME=$(basename "$FILEPATH")
-  EXTENSION="${FILENAME##*.}"
+  # get thumbnail for MP3
+  youtube-dl --write-thumbnail $address -o thumbnail.jpg
+  video_title="$(youtube-dl --get-title $address)"
 
-  ## Very much unused atm....
-  if [ $video_id != "*" ]; then
-    DATA=`curl -s https://gdata.youtube.com/feeds/api/videos/$video_id?v=2`
-    PUBLISHED=`echo $DATA | php -r 'print simplexml_load_file("php://stdin")->published;' | sed 's/\..*Z/Z/'`
-    AUTHOR=`echo $DATA | php -r 'print simplexml_load_file("php://stdin")->author->name;'`
-    TITLE=`echo $DATA | php -r '$x = simplexml_load_file("php://stdin"); $ns = $x->getNameSpaces(true); $m = $x->children($ns["media"]); print $m->group->title;'`
-    DESCRIPTION=`echo $DATA | php -r '$x = simplexml_load_file("php://stdin"); $ns = $x->getNameSpaces(true); $m = $x->children($ns["media"]); print $m->group->description;'`
-    COMMENT="http://www.youtube.com/watch?v=$YOUTUBE_ID"
-    ALBUM=$AUTHOR
+  artist="$(echo $video_title |awk -NF '-' '{print$1}' |sed -e 's/\[.*//g' -e 's/  */ /g' -e 's/^ *\(.*\) *$/\1/')"
+  title="$(echo $video_title |awk -NF '-' '{print$2}' |sed -e 's/\[.*//g' -e 's/  */ /g' -e 's/^ *\(.*\) *$/\1/')"
 
-    if [ "$1" ]; then
-      TITLE="$AUTHOR: $TITLE"
-      AUTHOR=$1
-      ALBUM=$1
-    fi
-  fi
+  # download the FLV stream
+  youtube-dl -o "$video_title".flv $address
 
-    # adding thumbnail to the MP3
-    thumb="$(youtube-dl --write-thumbnail $address |grep "to:.*" |awk '{for(i=6;i<=NF;i++) printf $i" "}')"
-    set -- $thumb; thumb=$*
-    mv "$thumb" 'thumbnail.jpg'
+  ffmpeg -i "$video_title".flv         \
+        -acodec libmp3lame             \
+        -ab 320k                       \
+        -ac 2                          \
+        -vn                            \
+        -y "$video_title".mp3
 
-    video_title="$(youtube-dl --get-title $address)"
+  # add image with LAME since FFMPEG changes too much....
+  lame --preset insane -V0 --id3v2-only --ignore-tag-errors \
+       --ti 'thumbnail.jpg'                                 \
+       --ta "$artist"                                       \
+       --tt "$title"                                        \
+       --tv "TPE2=${artist}"                                \
+       "$video_title".mp3 "${dir}/${video_title}.mp3"
 
-    author="$(echo $video_title |awk -NF '-' '{print$1}' |sed -e 's/\[.*//g' -e 's/  */ /g' -e 's/^ *\(.*\) *$/\1/')" 
-    title="$(echo $video_title |awk -NF '-' '{print$2}' |sed -e 's/\[.*//g' -e 's/  */ /g' -e 's/^ *\(.*\) *$/\1/')" 
-
-        # download the FLV stream
-        youtube-dl -o "$video_title".flv $address
-
-    ffmpeg -i "$video_title".flv          \
-           -i "thumbnail.jpg"             \
-           -metadata author="$author"     \
-           -metadata title="$title"       \
-           -acodec libmp3lame             \
-           -ac 2                          \
-           -ab 320k                       \
-           -vn                            \
-           -y "$video_title".mp3
-
-        # untested
-       if [ -z "$dir" ]; then
-         if [[ ! -d $dir ]]; then
-           echo "Creating directory $dir"
-           echo mkdir -p $dir
-         fi
-       fi
-
-    cp "$video_title".mp3 $dir
-
-    rm -f "$video_title".flv "$video_title".mp3 "thumbnail.jpg" *.{webm,mp4}
-else
-    echo "Sorry but you seemed to broken the interwebs."
-fi
+  rm -f "$video_title".flv thumbnail.jpg *.{webm,mp4}
+} || {
+  echo "Sorry but you seemed to broken the interwebs."
+}
