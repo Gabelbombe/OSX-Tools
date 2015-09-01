0b4e5a5d4b6f5131077327b59a98098175e30fef
Jon Daniel <ehime@Jons-MacBook-Pro.local>
2015-09-01 13:46:17 -0700

    was deprecated

--- env-setup.sh
+++ env-setup.sh
@@ -17,15 +17,15 @@ brew install coreutils
 brew tap homebrew/dupes
 brew install binutils
 brew install diffutils
-brew install ed --default-names
-brew install findutils --default-names
+brew install ed --with-default-names
+brew install findutils --with-default-names
 brew install gawk
-brew install gnu-indent --default-names
-brew install gnu-sed --default-names
-brew install gnu-tar --default-names
-brew install gnu-which --default-names
-brew install gnutls --default-names
-brew install grep --default-names
+brew install gnu-indent --with-default-names
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-which --with-default-names
+brew install gnutls --with-default-names
+brew install grep --with-default-names
 brew install gzip
 brew install screen
 brew install watch
